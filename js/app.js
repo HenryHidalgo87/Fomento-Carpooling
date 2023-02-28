87e8397facb75a9c57e958e44bd47d05ab0f2a84
Henry <henryhidalgo870@gmail.com>
2023-02-28 17:21:18 -0500

    Version one

---
+++ js/app.js
@@ -0,0 +1,61 @@
+window.sr = ScrollReveal();
+
+/*-------Header------------*/
+sr.reveal('.navbar', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-100px',
+  });
+
+sr.reveal('#banner', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-100px',
+  });
+
+  sr.reveal('#header', { 
+    easing: 'ease-in',
+    duration: 2000, 
+    delay: 1500
+  });
+
+
+/*--SEPARADOR FRASE--*/ 
+sr.reveal('#test1', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-300px',
+  });
+
+  sr.reveal('#test2', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '300px',
+    // viewFactor: 0.2
+  });
+
+
+/*----------SECCION SERVICIOS-----*/
+
+
+/*---------QUIENES SOMOS----------*/
+sr.reveal('#testimonial', {
+  duration: 2000,
+  origin: 'left',
+  distance: '300px',
+  viewFactor: 0.2
+});
+
+
+/*----------SECCION ACERCA-----*/
+
+/*----------SECCION ACERCA-----*/
+
+
+/*----------CONTACT-----------*/
+sr.reveal('#agende', {
+  duration: 3000,
+  origin: 'bottom',
+  distance: '100px',
+});
+
